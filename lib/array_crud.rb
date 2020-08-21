12472d3457bd524c9deb9c7b94a204d38575aa4f
elizabeth goodman <egoodman1180@icloud.com>
2020-08-21 23:32:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,14 +23,13 @@ def remove_element_from_start_of_array(array)
   end
 
 def retrieve_element_from_index(array, index_number)
-  array.index_number("red fish") # whats the purpose of index_number? <<<<<<<< "why are u using red fish is more better question..." its not a vartiable... its a string literal.... what are the variables available to you rn in this method.... and..... okay. well i caqn tell u its not red fish
-  im here to help, 
+  array.index  
   end
 
 def retrieve_first_element_from_array(array)
   array[0]
   end
 def retrieve_last_element_from_array(array)
-  array[3] # you cant guarantee that the array length will be 4 (array indexes start at 0)
+  array[3] 
   end
 
