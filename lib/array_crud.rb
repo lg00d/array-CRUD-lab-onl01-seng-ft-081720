5ddec216dab41bfc16396b29e5056061ebb396b1
elizabeth goodman <egoodman1180@icloud.com>
2020-08-21 23:41:50 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,30 +7,29 @@ def create_an_array
   end
 
 def add_element_to_end_of_array(array, element)
-  << "moo fish"
+  array<< element
   end
 
 def add_element_to_start_of_array(array, element)
-  unshift ("yellow fish")
+  array.unshift(element)
   end
 
 def remove_element_from_end_of_array(array)
-  pop
-  end
+ array.pop
+  end 
 
 def remove_element_from_start_of_array(array)
-  famous_fish.shift
+  array.shift
   end
 
 def retrieve_element_from_index(array, index_number)
-  [0]
+  array[index_number]
   end
 
 def retrieve_first_element_from_array(array)
-  [0]
+  array.first
   end
-
 def retrieve_last_element_from_array(array)
-  [3]
+  array.last
   end
 
