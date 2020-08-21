e3df841d2bc3552ee8a46ad6cba97b9e6be20713
elizabeth goodman <egoodman1180@icloud.com>
2020-08-21 23:25:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
   end
 
 def remove_element_from_start_of_array(array)
-  two_fish = array.pop #Where is two_fish coming from???? the first tab told me that you have to state th
+  two_fish = array.pop #Where is two_fish coming from???? the first tab told me that you have to state th sorry lolsk 
   end
 
 def retrieve_element_from_index(array, index_number)
